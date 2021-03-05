38fb68c72861c02a8aac4684fdf89ecaf0c6f51e
RyanK2 <16060695@student.hhs.nl>
2021-03-05 18:23:43 +0100

    finished

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Challenge2HER_RyanKaria
  Challenge 2 XT2 Extra Klas Ryan Karia 16060695
+
+Klok op huidige tijd met enkele animaties in de vorm van wolken. 3 breakpoints en flexbox geimplementeerd
---
+++ script.js
@@ -0,0 +1,46 @@
+function giveTime() {
+var today = new Date();
+var hour = today.getHours();
+var min = today.getMinutes();
+var sec = today.getSeconds();
+var day = String(today.getDate()).padStart(2, '0');
+var month = String(today.getMonth() + 1).padStart(2, '0');
+var year = today.getFullYear();
+
+if (sec.toString().length == 1) {
+    sec = "0" + sec;
+}
+
+if (min.toString().length == 1) {
+    min = "0" + min;
+}
+
+if (hour.toString().length == 1) {
+    hour = "0" + hour;
+}
+
+if(hour >= 24) {
+    hour = 0;
+}
+
+document.getElementById('time').innerHTML = hour + ":" + min + ":" + sec + " CET";
+document.getElementById('date').innerHTML = day + "/" + month + "/" + year;
+var t = setTimeout(giveTime, 100);
+
+if(hour < 7 ) {
+document.getElementById('background').style.backgroundImage = 'url(images/night.svg)';
+document.body.style.backgroundColor = "#537C84";
+
+}
+
+if(hour >= 7 && hour < 12) {
+ document.getElementById('background').style.backgroundImage = 'url(images/day.svg)';
+document.body.style.backgroundColor = "#B3DDC2";
+}
+
+if(hour >= 12 && hour <= 23) {
+ document.getElementById('background').style.backgroundImage = 'url(images/afternoon.svg)';
+ document.body.style.backgroundColor = "#F58F3C";
+}
+
+};
